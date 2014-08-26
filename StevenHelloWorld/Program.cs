0b309b19484e64666979faefa424a1c9a4acd04c
steven-pretorius <steven.pretorius@kingprice.co.za>
2014-08-26 15:05:38 +0200

    Hello world commit for KP training

---
+++ StevenHelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StevenHelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!!!!");
+            Console.WriteLine("by Steven Pretorius");
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
